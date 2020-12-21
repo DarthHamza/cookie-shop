43d8537f149265b38aba3e859476853ce3920a8e
DarthHamza <hamzamakia@gmail.com>
2020-12-21 18:17:06 +0300

    completed Header Chapter from React 1 workshop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+const styles = {
+  text: { textAlign: "center"},
+  shopImage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%",
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={styles.text}>Cookies and Beyond</h1>
+
+      <h4 style={styles.text}>Where cookie maniacs gather</h4>
+      <img
+        style = {styles}
+        alt="cookie shop"
+        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+      />
     </div>
   );
 }
