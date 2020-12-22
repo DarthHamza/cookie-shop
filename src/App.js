55e2a467c10ebbf08319e045b3adcc71804df5b6
DarthHamza <hamzamakia@gmail.com>
2020-12-22 10:29:58 +0300

    Completed Cookie List Chapter from React 1 workshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react";
+// Data
+import cookies from "./cookies";
+// Styling
+import styles from "./styles";
 
-const styles = {
-  text: { textAlign: "center"},
-  shopImage: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "50%",
-  }
-}
+
 function App() {
+  const cookieList = cookies.map(cookie => (
+    <div style={ styles.cookie }>
+      <img
+        alt={ cookie.name }
+        src={ cookie.image }
+        style={styles.cookieImage}
+      />
+      <p style={styles.text}>{ cookie.name }</p>
+      <p style={styles.text}>{ cookie.price } KD</p>
+    </div>
+  ))
   return (
     <div>
       <h1 style={styles.text}>Cookies and Beyond</h1>
-
       <h4 style={styles.text}>Where cookie maniacs gather</h4>
       <img
         style = {styles}
         alt="cookie shop"
         src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
       />
+      <div style={ styles.list }>
+        { cookieList }
+      </div>
     </div>
   );
 }
